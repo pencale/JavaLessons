2dbabd5be358ed14c953cce08eaf865e950205af
pencale <pencha@stud-086.itcwin.com>
2015-02-26 18:27:34 +0300

    added java sources

---
+++ JavaLessons/src/start/Lesson1.java
@@ -0,0 +1,10 @@
+package start;
+public class Lesson1 {
+
+
+	public static void main(String[] args){
+		System.out.println("Shake your Ass!!");
+	}
+	
+	
+}
---
+++ JavaLessons/src/start/begin/Lesson2.java
@@ -0,0 +1,8 @@
+package start.begin;
+
+public class Lesson2 {
+
+	public static void main(String[] args) {
+	System.out.println("Take your time");	
+	}
+}
